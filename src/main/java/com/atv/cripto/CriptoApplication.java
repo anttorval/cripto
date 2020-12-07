3bedcab87fe1ddddadcae55494da0a04381b1ed0
atoro <anttorval2@gmail.com>
2020-12-07 12:44:30 +0100

    dev initial commit

---
+++ src/main/java/com/atv/cripto/CriptoApplication.java
@@ -0,0 +1,13 @@
+package com.atv.cripto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptoApplication.class, args);
+	}
+
+}
